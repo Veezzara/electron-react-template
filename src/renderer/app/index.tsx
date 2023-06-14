c8901a4ea42787907c23bdb2e2dfd566d8c3d24b
Max Zhabin <veezzara@gmail.com>
2023-06-14 14:23:32 +0400

    type fix

--- src/renderer/app/index.tsx
+++ src/renderer/app/index.tsx
@@ -1,5 +1,5 @@
 import CredentialsDemo from "../credentials-demo/credentials-demo";
-import HttpClientDemo from "../http-client-demo/http-cilent-demo";
+import HttpClientDemo from "../http-client-demo/http-client-demo";
 
 const App = () => {
   return (
@@ -8,8 +8,8 @@ const App = () => {
       <span>{`This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`}</span>
       <br />
       <br />
-      <CredentialsDemo />
-      <HttpClientDemo />
+      {/* <CredentialsDemo /> */}
+      {/* <HttpClientDemo /> */}
     </main>
   );
 };
