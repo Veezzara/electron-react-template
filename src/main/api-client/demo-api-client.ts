4bf5b18fa315008d33e479b0d31d7cc1dfd26ef8
Max Zhabin <veezzara@gmail.com>
2023-06-14 14:33:39 +0400

    write file example

--- src/main/api-client/demo-api-client.ts
+++ src/main/api-client/demo-api-client.ts
@@ -1,5 +1,8 @@
 import { AxiosClient } from "../http-client/axios-client";
 import { HttpMethod, IHttpClient } from "../http-client/http-client.interface";
+import fs from "fs";
+import os from "os";
+import path from "path";
 
 export class DemoApiClient {
   private httpClient: IHttpClient;
@@ -9,9 +12,17 @@ export class DemoApiClient {
   }
 
   getPosts = async () => {
-    return this.httpClient.request<undefined, any>(
+    const response = await this.httpClient.request<undefined, any>(
       HttpMethod.GET,
       "posts",
     );
+
+    fs.writeFile(path.join(os.homedir(), "posts.txt"), JSON.stringify(response), (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+
+    return response;
   };
 }
